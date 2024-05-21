e0db8d414cb214c83c50bbca0532eea27a7f382c
Alan Barbera <alanbarbera02@gmail.com>
2024-05-21 00:18:10 -0300

    add: variables a Producto y design template Producto

--- project/producto/models.py
+++ project/producto/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from decimal import Decimal
 # Create your models here.
 
 class ProductoCategoria(models.Model):
     '''Categorias de productos'''
-    nombre = models.CharField(max_length=200, unique=True)
+    nombre = models.CharField(max_length=100, unique=True)
+    codigo = models.PositiveIntegerField(null=False, blank=False, unique=True, default=1)
+    puntos = models.PositiveIntegerField(blank=True, default=1)
     descripcion = models.CharField(max_length=300, null=True, blank=True, verbose_name="descripción")
+    precio_revista = models.DecimalField(max_digits=300, decimal_places=2, default=Decimal('0.00'))
+    # precio_consultora = models.DecimalField(max_digits=300, decimal_places=2)
 
     def __str__(self):
         '''Representa una instacia del modelo como una cadena de texto'''
@@ -14,4 +18,3 @@ class ProductoCategoria(models.Model):
     class Meta:
         verbose_name = "Categoría de productos"
         verbose_name_plural = "Categorías de productos"
-
