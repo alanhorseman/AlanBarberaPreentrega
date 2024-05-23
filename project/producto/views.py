25f66726635764876cf910889890a47109b895b6
Alan Barbera <alanbarbera02@gmail.com>
2024-05-23 20:25:48 -0300

    se elimino lineas de codigo comentadas innecesarias

--- project/producto/views.py
+++ project/producto/views.py
@@ -43,33 +43,3 @@ class ProductoCategoriaUpdate(LoginRequiredMixin, UpdateView):
 class ProductoCategoriaDelete(LoginRequiredMixin, DeleteView):
     model = models.ProductoCategoria
     success_url = reverse_lazy("producto:productocategoria_list")
-
-# class ProductoCategoriaList(ListView):
-#     model = models.ProductoCategoria
-
-
-# class ProductoCategoriaList(ListView):
-#     model = models.ProductoCategoria
-#     template_name = 'producto/productocategoria_list.html'  # Especifica el nombre del template
-#     context_object_name = 'categorias'  # Especifica el nombre del objeto en el contexto
-
-#     def get_queryset(self):
-#         queryset = super().get_queryset()  # Obtiene el queryset base
-
-#         # Obtiene la consulta de la URL
-#         consulta = self.request.GET.get('consulta')
-
-#         if consulta:
-#             queryset = queryset.filter(nombre__icontains=consulta)
-
-#         return queryset
-
-# def homee(request):
-#     consulta = request.GET.get('consulta')
-#     categorias = models.ProductoCategoria.objects.all()  # Todas las categorías por defecto
-
-#     if consulta:  # Si se proporciona una consulta
-#         categorias = models.ProductoCategoria.objects.filter(nombre__icontains=consulta)
-
-#     context = {'categorias': categorias}
-#     return render(request, 'producto/index.html', context)
